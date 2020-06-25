5a7db4f67ead30a36b4678a4e0342bd66994abd8
Amélie <ameliechevalier3006@gmail.com>
2020-06-25 10:25:25 +0200

    display ad info

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 import firebase from '../firebase';
 import AdsItemSmall from './AdsItemSmall'
 import Footer from './Footer';
+import '../styles/Ads.css';
 
 const Ads = () => {
 
   const [ads, setAds] = useState([])
 
   useEffect(() => {
-    const loadAd = firebase.firestore().collection('adds').onSnapshot(s => {
+    const unsubscribe = firebase.firestore().collection('adds').onSnapshot(s => {
       setAds(s.docs.map(ad => {
         return {id: ad.id, ...ad.data()}
       }))
     })
 
-    return () => loadAd()
+    return () =>unsubscribe()
   }, [])
  
     return (
-        <div>
+        <div className='ads ads-container'>
             {ads.map((ad)=> <AdsItemSmall ad={ad}/>)}
             <Footer/>
         </div>
