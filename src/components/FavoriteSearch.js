036c6f00745f353aa064e4be60fba3cf6d7a2d39
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-25 21:57:57 +0200

    ok

--- src/components/FavoriteSearch.js
+++ src/components/FavoriteSearch.js
@@ -2,15 +2,10 @@ import React, { useContext } from 'react';
 import DatasContext from './DatasContext';
 import Searchbar from "../components/Searchbar";
 
-
 const FavoriteSearch = () => {
-    const {places,specialties, dates, durations} = useContext(DatasContext);
-
-    console.log(places);
-
     return (
         <main>
-            {places}
+            
         </main>
     );
 };
