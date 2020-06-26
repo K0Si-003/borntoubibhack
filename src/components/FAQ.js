13f9ca0f61fcda3d83fff793450411dff9663a38
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-26 04:05:35 +0200

    hello

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
-const FAQ = () => {
+const FAQ = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
     return (
         <main>
-
+            <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+            <Collapse isOpen={isOpen}>
+                <Card>
+                    <CardBody>
+                    Anim pariatur cliche reprehenderit,
+                    enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                    anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident.
+                    </CardBody>
+                </Card>
+            </Collapse>
         </main>
     );
 };
--- src/components/FavoriteSearch.js
+++ src/components/FavoriteSearch.js
@@ -1,5 +1,13 @@
 import React, {useEffect,useState} from 'react';
 import Searchbar from "../components/Searchbar";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import '../styles/favorites.css'
 
 const FavoriteSearch = (props) => {
     const [searches, setSearches] = useState([])
@@ -10,9 +18,38 @@ const FavoriteSearch = (props) => {
     
     return (
         <main>
-            <ul>
-            {searches.map(s => <li key={s.id}>{s.place}</li>)}
-            </ul>
+            {searches.map((search, index) => {
+                return (
+                        <Card className='card-search'>
+                            <CardContent>
+                                <Typography color="textPrimary" gutterBottom>
+                                Recherche {index + 1}
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                </Typography>
+                                <Typography color="textSecondary">
+                                {search.place}
+                                </Typography>
+                                <Typography color="textSecondary">
+                                {search.specialty}
+                                </Typography>
+                                <Typography color="textSecondary">
+                                {search.duration}
+                                </Typography>
+                                <Typography color="textSecondary">
+                                {search.date}
+                                </Typography>
+
+                            </CardContent>
+                            <CardActions style={{textAlign: 'center'}}>
+                            <Link to="/annonces/search">
+                                <Button  size="small">Lancer ma recherche</Button>
+                            </Link>
+                            </CardActions>
+                        </Card>
+                )
+            })}
+
         </main>
     );
 };
