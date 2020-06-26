f30c0f4f80e76b7276ee1806bc703879a27e0839
Kosi <hugo.pioda@gmail.com>
2020-06-26 11:34:06 +0200

    update style

--- src/components/FavoriteSearch.js
+++ src/components/FavoriteSearch.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,48 +8,49 @@ import { Link } from 'react-router-dom';
 import '../styles/favorites.css'
 
 const FavoriteSearch = (props) => {
-    const [searches, setSearches] = useState([])
+  const [searches, setSearches] = useState([])
 
-    useEffect(() => {
-       setSearches(JSON.parse(localStorage.getItem('localSearch')) || []);
-    }, [])
-    
-    return (
-        <main>
-            {searches.map((search, index) => {
-                return (
-                        <Card className='card-search'>
-                            <CardContent>
-                                <Typography color="textPrimary" gutterBottom>
-                                Recherche {index + 1}
-                                </Typography>
-                                <Typography variant="h5" component="h2">
-                                </Typography>
-                                <Typography color="textSecondary">
-                                {search.place}
-                                </Typography>
-                                <Typography color="textSecondary">
-                                {search.specialty}
-                                </Typography>
-                                <Typography color="textSecondary">
-                                {search.duration}
-                                </Typography>
-                                <Typography color="textSecondary">
-                                {search.date}
-                                </Typography>
+  useEffect(() => {
+    setSearches(JSON.parse(localStorage.getItem('localSearch')) || []);
+  }, [])
 
-                            </CardContent>
-                            <CardActions style={{textAlign: 'center'}}>
-                            <Link to="/annonces/search">
-                                <Button  size="small">Lancer ma recherche</Button>
-                            </Link>
-                            </CardActions>
-                        </Card>
-                )
-            })}
+  return (
+    <main className='fav-container'>
+      <h1 className='fav-title'>Mes recherches</h1>
+      {searches.map((search, index) => {
+        return (
+          <Card className='card-search'>
+            <CardContent>
+              <Typography color="textPrimary" gutterBottom>
+                Recherche {index + 1}
+              </Typography>
+              <Typography variant="h5" component="h2">
+              </Typography>
+              <Typography color="textSecondary">
+                {search.place}
+              </Typography>
+              <Typography color="textSecondary">
+                {search.specialty}
+              </Typography>
+              <Typography color="textSecondary">
+                {search.duration}
+              </Typography>
+              <Typography color="textSecondary">
+                {search.date}
+              </Typography>
 
-        </main>
-    );
+            </CardContent>
+            <CardActions style={{ textAlign: 'center' }}>
+              <Link to="/annonces/search">
+                <Button size="small">Lancer ma recherche</Button>
+              </Link>
+            </CardActions>
+          </Card>
+        )
+      })}
+
+    </main>
+  );
 };
 
 export default FavoriteSearch;
