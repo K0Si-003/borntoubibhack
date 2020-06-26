b6ec4d196ca4e8f919cef34c2ec91ddc01f805d2
Kosi <hugo.pioda@gmail.com>
2020-06-26 10:50:15 +0200

    clean faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import '../styles/FAQ.css';
 
 const FAQ = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
-    return (
-        <main>
-     <ExpansionPanel style={{width: '70%', margin: '20px auto'}}>
+  return (
+    <main className='faq-container'>
+      <h1 className='faq-title'>FAQ</h1>
+      <ExpansionPanel style={{ width: '70%', margin: '20px auto' }}>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography style={{fontWeight: 'bold'}}>Comment puis-je effectuer un remplacement?</Typography>
+          <Typography style={{ fontWeight: 'bold' }}>Comment puis-je effectuer un remplacement?</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            Vous pouvez filtrer les annonces de remplacement grâce à une recherche avancée incluant la spécialité de médecine, le lieu géographique, la date de début et la durée de remplacement souhaitée. 
+            Vous pouvez filtrer les annonces de remplacement grâce à une recherche avancée incluant la spécialité de médecine, le lieu géographique, la date de début et la durée de remplacement souhaitée.
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
 
-      <ExpansionPanel style={{width: '70%', margin: '20px auto'}}>
+      <ExpansionPanel style={{ width: '70%', margin: '20px auto' }}>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography style={{fontWeight: 'bold'}}>Est-ce que les démarches de remplacement sont inclues?</Typography>
+          <Typography style={{ fontWeight: 'bold' }}>Est-ce que les démarches de remplacement sont inclues?</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            Vous pouvez filtrer les annonces de remplacement grâce à une recherche avancée incluant la spécialité de médecine, le lieu géographique, la date de début et la durée de remplacement souhaitée. 
+            Vous pouvez filtrer les annonces de remplacement grâce à une recherche avancée incluant la spécialité de médecine, le lieu géographique, la date de début et la durée de remplacement souhaitée.
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
 
 
-        </main>
-    );
+    </main>
+  );
 };
 
 export default FAQ;
